6922df651465d508e76e0076f2ccf2526fcfd0ee
ctamani <133699087+ctamani@users.noreply.github.com>
2025-04-16 16:03:53 -0500

    Created map page with an InfoWindow feature

---
+++ js/map.js
@@ -0,0 +1,47 @@
+// Initialize and add the map
+function initMap() {
+  // 1. Set Location: IIT Location
+  const location = {lat: 41.8349, lng: -87.6270 };
+
+  // 2. Create the map
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: location,
+    mapTypeId: 'roadmap',
+    mapTypeControl: true,
+    zoomControl: true,
+    streetViewControl: false,
+  });
+
+  // 3. Add a marker
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: "We are here!",
+    icon: {
+      url: 'images/smollcat.png',
+      scaledSize: new google.maps.Size(50, 50), // Width x Height in pixels
+    },
+  });
+
+  // ___Feature: InfoWindow___
+  // 4. Add an InfoWindow
+  const infoWindow = new google.maps.InfoWindow({
+    content: "<h2>IIT Main Campus</h2><p>Come visit us in Chicago!</p>"
+  });
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
+
+ new google.maps.Circle({
+  strokeColor: '#ff8df7',
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: '#ff8df7',
+  fillOpacity: 0.15,
+  map,
+  center: location,
+  radius: 2000
+});
+
+}
